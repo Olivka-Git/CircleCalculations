77bef4925c3178e0f791b39eb79e803ac951841b
David <davidka.khachatryan@bk.ru>
2025-10-22 11:31:08 +0300

    1 коммит - расчет длины окружности

---
+++ Practive_with_GitBash/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CircleCalculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа для расчёта параметров круга");
+            Console.Write("Введите радиус: ");
+
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out double radius))
+            {
+                Console.WriteLine($"Радиус: {radius}");
+                double circumference = CalculateCircumference(radius);
+                Console.WriteLine($"Длина окружности: {circumference:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибочка! Введите корректное число.");
+            }
+
+            Console.ReadKey();
+        }
+        static double CalculateCircumference(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+    }
+}
---
+++ Practive_with_GitBash/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Practive_with_GitBash")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Practive_with_GitBash")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("01af86ff-5720-41d4-bc43-769dc87a3d51")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
