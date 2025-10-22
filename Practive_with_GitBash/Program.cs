f4d5fa249828a586f0846286b0ea79e8c74fe4c3
David <davidka.khachatryan@bk.ru>
2025-10-22 11:58:22 +0300

    2 коммит Rectangle - расчет площади

--- Practive_with_GitBash/Program.cs
+++ Practive_with_GitBash/Program.cs
@@ -17,11 +17,10 @@ namespace CircleCalculations
                 double.TryParse(widthInput, out double width))
             {
                 double perimeter = CalculatePerimeter(length, width);
+                double area = CalculateRectangleArea(length, width);
+
                 Console.WriteLine($"Периметр прямоугольника: {perimeter:F2}");
-            }
-            else
-            {
-                Console.WriteLine("Ошибка! Введите корректные числа.");
+                Console.WriteLine($"Площадь прямоугольника: {area:F2}");
             }
 
             Console.ReadKey();
@@ -31,6 +30,10 @@ namespace CircleCalculations
         static double CalculatePerimeter(double length, double width)
         {
             return 2 * (length + width);
+        }
+        static double CalculateRectangleArea(double length, double width)
+        {
+            return length * width;
         }
     }
 }
